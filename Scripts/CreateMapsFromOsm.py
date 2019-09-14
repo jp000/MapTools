8443c014581595fe031b81f310b5fd9ec32f323b
softxfr <softxfr@gmail.com>
2019-09-14 16:53:35 +0200

    improvements

--- Scripts/CreateMapsFromOsm.py
+++ Scripts/CreateMapsFromOsm.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 
-import os, sys, stat
+import os
 import re
 import shlex
 import shutil
 import subprocess
+from datetime import datetime
 from os import path
-from datetime import datetime, timedelta
 
 # http://garmin.opentopomap.org/
 # http://garmin.openstreetmap.nl/
@@ -33,14 +33,6 @@ if not os.path.exists(dnldDir):
     os.makedirs(dnldDir)
 
 
-#def fileOld(file_path):
-#    if not os.path.exists(file_path):
-#        return True
-#    days_ago = datetime.now() - timedelta(weeks=4)
-#    filetime = datetime.fromtimestamp(path.getctime(file_path))
-#    return filetime < days_ago
-
-
 def fileAge(file_path):
     if not os.path.exists(file_path):
         return 36500
@@ -50,39 +42,39 @@ def fileAge(file_path):
 def doOverpass(outputfilename, query, server=servers[2], bbox=None):
     tmp = os.path.join(os.path.dirname(outputfilename), 'Tmp.dat')
     if bbox is None:
-        data = 'data=[out:xml][timeout:600][maxsize:1073741824];{}'.format(query)
+        data = f'data=[out:xml][timeout:600][maxsize:1073741824];{query}'
     else:
         # data=[bbox];node[amenity=post_box];out body;&bbox=6.4380,43.6490,7.5696,46.375068 (w,s,e,n)
-        data = 'data=[out:xml][timeout:600][maxsize:1073741824][bbox];{}&bbox={}'.format(query, bbox)
+        data = f'data=[out:xml][timeout:600][maxsize:1073741824][bbox];{query}&bbox={bbox}'
 
     with open(tmp, 'w', encoding='utf-8') as fp:
         fp.writelines(data)
-        print('Created {}'.format(tmp))
-    cmd = r'"{3}" -O "{0}" --post-file="{1}" {2}'.format(outputfilename, tmp, server, wgetExe)
-    print('Execute {}'.format(cmd))
+        print(f'Created {tmp}')
+    cmd = fr'"{wgetExe}" -O "{outputfilename}" --post-file="{tmp}" {server}'
+    print(f'Execute {cmd}')
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     r = p.communicate()
     if p.returncode == 0:
-        print('Created {}'.format(outputfilename))
+        print(f'Created {outputfilename}')
         return True
     else:
-        print('Failed\n{}'.format(r[1].decode('latin-1')))
+        print(f'Failed\n{r[1].decode("latin-1")}')
         return False
 
 
 def downloadFile(outputfilename, remoteFilename):
     if fileAge(outputfilename) < 21:
-        print('File already OK {}'.format(outputfilename))
+        print(f'File already OK {outputfilename}')
         return True
-    cmd = r'"{2}" -O "{0}" "{1}"'.format(outputfilename, remoteFilename, wgetExe)
-    print('Execute {}'.format(cmd))
+    cmd = fr'"{wgetExe}" -O "{outputfilename}" "{remoteFilename}"'
+    print(f'Execute {cmd}')
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     r = p.communicate()
     if p.returncode == 0:
-        print('Created {}'.format(outputfilename))
+        print(f'Created {outputfilename}')
         return True
     else:
-        print('Failed\n{}'.format(r[1].decode('latin-1')))
+        print(f'Failed\n{r[1].decode("latin-1")}')
         return False
 
 
@@ -90,7 +82,7 @@ def mkgmap(inputfilename,
            javaJar=mkgmapJar,
            styleDir=os.path.join(StyleDir, r'e10'),
            typeFile=os.path.join(StyleDir, r'e10.typ'),
-           mapId=4360001,
+           mapId=0,
            mapDescription=None,
            hasRoute=True,
            outputDir=outDir):
@@ -98,56 +90,55 @@ def mkgmap(inputfilename,
     styleName = os.path.split(styleDir)[-1]
     name, ext = os.path.splitext(os.path.basename(inputfilename))
     if ext == '.args':
-        inputfilename = '-c "{}"'.format(inputfilename)
+        inputfilename = f'-c "{inputfilename}"'
         mapName = ''
         if mapDescription is None:
             mapDescription = 'Noname'
     else:
         if mapDescription is None:
             mapDescription = name + '_' + styleName
-        inputfilename = '"{}"'.format(inputfilename)
-        mapName = '--mapname={} '.format(mapId)
+        inputfilename = f'"{inputfilename}"'
+        mapName = f'--mapname={mapId} '
 
     route = '--route ' if hasRoute else ''
     if typeFile is None or typeFile == '':
         typeFile = ''
     else:
-        typeFile = '"{}"'.format(typeFile)
+        typeFile = f'"{typeFile}"'
 
-    cmd = r'java -Xmx4000m -jar "{0}" --gmapsupp --latin1 --index --add-pois-to-areas {8} --ignore-fixme-values {7}' \
-          r' --style-file="{1}" --family-id=1000 --product-id=1 {2}' \
-          r' --description="{3}" --output-dir="{4}" {5} {6}' \
-        .format(javaJar, styleDir, mapName, mapDescription, outputDir, inputfilename, typeFile, route, poiExclude)
+    cmd = fr'java -Xmx4000m -jar "{javaJar}" --gmapsupp --latin1 --index --add-pois-to-areas {poiExclude} --ignore-fixme-values {route} --style-file="{styleDir}" --family-id=1000 --product-id=1 {mapName} --description="{mapDescription}" --output-dir="{outputDir}" {inputfilename} {typeFile}'
 
     return executeJava(cmd, mapDescription, outputDir)
 
 
 def executeJava(cmd, mapName, outputDir):
-    print('Execute {}'.format(cmd))
+    print(f'Execute {cmd}')
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     r = p.communicate()
     if p.returncode == 0:
         resultFilename = os.path.abspath(os.path.join(outputDir, mapName + '.img'))
-        print('Created {} from {}'.format(resultFilename, os.path.join(outputDir, 'gmapsupp.img')))
+        print(f'Created {resultFilename} from {os.path.join(outputDir, "gmapsupp.img")}')
         shutil.copyfile(os.path.join(outputDir, 'gmapsupp.img'), resultFilename)
         return resultFilename
     else:
-        print('Failed\n{}'.format(r[1].decode('latin-1')))
+        print(f'Failed\n{r[1].decode("latin-1")}')
         return None
 
 
 def mkgsplit(inputfilename,
              javaJar=splitterJar,
-             outputDir=outDir):
-    cmd = r'java -Xmx4000m -jar "{0}" --output-dir="{1}" "{2}"'.format(javaJar, outputDir, inputfilename)
-    print('Execute {}'.format(cmd))
+             outputDir=outDir,
+             description='JP Map',
+             mapId=43600000):
+    cmd = fr'java -Xmx4000m -jar "{javaJar}" --max-nodes=2400000 --mapid={mapId} --description={description} --output-dir="{outputDir}" "{inputfilename}"'
+    print(f'Execute {cmd}')
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     r = p.communicate()
     if p.returncode == 0:
-        print('Created {}'.format(os.path.join(outputDir, 'template.args')))
+        print(f'Created {os.path.join(outputDir, "template.args")}')
         return os.path.join(outputDir, 'template.args')
     else:
-        print('Failed\n{}'.format(r[1].decode('latin-1')))
+        print(f'Failed\n{r[1].decode("latin-1")}')
         return None
 
 
@@ -158,27 +149,34 @@ def mkMap(osm, myId, query,
     return mkgmap(osm, styleDir=styleDir, typeFile=typeFile, mapId=myId)
 
 
-def Replace(data, startId):
-    global newid
+# def Replace(data, startId):
+#     global newid
+#
+#     def _replace(m):
+#         global newid
+#         s = 'mapname: {:08d}'.format(newid)
+#         newid += 1
+#         return s
+#
+#     pattern = re.compile(r'mapname: \d+')
+#     newid = startId
+#     return pattern.sub(lambda m: _replace(m.group(0)), data), newid
 
-    def _replace(m):
-        global newid
-        s = 'mapname: {:08d}'.format(newid)
-        newid += 1
-        return s
-    pattern = re.compile(r'mapname: \d+')
-    newid = startId
-    return pattern.sub(
-        lambda m: _replace(m.group(0)), data), newid
-
-
-def updateMapId(file, myId):
-    with open(file, 'r+') as fp:
+def FindLastMapId(file):
+    pattern = re.compile(r'mapname: (\d+)')
+    with open(file, 'r') as fp:
         data = fp.read()
-        d = Replace(data, myId)
-        fp.seek(0, 0)
-        fp.write(d[0])
-    return d[1]
+    matches = pattern.findall(data)
+    return int(matches[-1])
+
+
+# def updateMapId(file, myId):
+#     with open(file, 'r+') as fp:
+#         data = fp.read()
+#         d = Replace(data, myId)
+#         fp.seek(0, 0)
+#         fp.write(d[0])
+#     return d[1]
 
 
 def cleanup(root=outDir):
@@ -198,29 +196,85 @@ def cleanup(root=outDir):
 
 
 def ProcessGeoFabrik(urlRoot, osmFile, startId):
-    if type(osmFile) is list or type(osmFile) is tuple:
-        pass
-    else:
+    _startId = startId
+    if type(osmFile) is not list and type(osmFile) is not tuple:
         osmFile = (osmFile,)
     for _osmfile in osmFile:
         m = re.split('-latest', _osmfile)
         mapname = m[0]
         osmpath = os.path.join(dnldDir, _osmfile)
+        template = None
         if downloadFile(osmpath, urlRoot + _osmfile):
-            template = mkgsplit(osmpath)
+            template = mkgsplit(osmpath, description=mapname, mapId=startId)
+
             if template is not None:
-                # startId = updateMapId(template, startId)
-                # mkgmap(template, styleDir=os.path.join(StyleDir, 'e10'), typeFile=os.path.join(StyleDir, 'e10.typ'), mapDescription='E10_' + mapName)
-                startId = updateMapId(template, startId)
+                lastId = FindLastMapId(template)
+            #                _startId = startId
+            #                lastId = updateMapId(template, startId)
                 mkgmap(template, styleDir=os.path.join(StyleDir, 'e20x'),
                        typeFile=os.path.join(StyleDir, 'e20x.typ'), mapDescription='E20_' + mapname)
-    return startId
+            startId = lastId + 1
+    return lastId - 1, _startId
+
+
+def GeoFabrik():
+    lst = [
+        (0, 42300000, 'https://download.geofabrik.de/africa/', 'mauritius-latest.osm.pbf'),
+        (0, 42610000, 'https://download.geofabrik.de/africa/', 'madagascar-latest.osm.pbf'),
+        (0, 43100000, 'https://download.geofabrik.de/europe/', 'netherlands-latest.osm.pbf'),
+        (0, 43200000, 'https://download.geofabrik.de/europe/', 'belgium-latest.osm.pbf'),
+        (0, 43300000, 'https://download.geofabrik.de/europe/', 'france-latest.osm.pbf'),
+        (0, 43400000, 'https://download.geofabrik.de/europe/', 'spain-latest.osm.pbf'),
+        (0, 43570000, 'https://download.geofabrik.de/europe/', 'cyprus-latest.osm.pbf'),
+        (0, 43900000, 'https://download.geofabrik.de/europe/', 'italy-latest.osm.pbf'),
+        (1, 44100000, 'https://download.geofabrik.de/europe/', 'switzerland-latest.osm.pbf'),
+        (0, 44300000, 'https://download.geofabrik.de/europe/', 'austria-latest.osm.pbf'),
+        (0, 44400000, 'https://download.geofabrik.de/europe/', 'great-britain-latest.osm.pbf'),
+        (0, 44900000, 'https://download.geofabrik.de/europe/', 'germany-latest.osm.pbf'),
+        (0, 46100000, 'https://download.geofabrik.de/australia-oceania/', 'australia-latest.osm.pbf'),
+        (0, 46400000, 'https://download.geofabrik.de/australia-oceania/', 'new-zealand-latest.osm.pbf'),
+        (0, 00000000, 'https://download.geofabrik.de/xxx/', 'xxx.pbf'),
+
+        (0, 43300000, 'https://download.geofabrik.de/europe/france/', ('franche-comte-latest.osm.pbf',
+                                                                       'haute-normandie-latest.osm.pbf',
+                                                                       'basse-normandie-latest.osm.pbf',
+                                                                       'ile-de-france-latest.osm.pbf',
+                                                                       'nord-pas-de-calais-latest.osm.pbf',
+                                                                       'picardie-latest.osm.pbf',
+                                                                       'provence-alpes-cote-d-azur-latest.osm.pbf',
+                                                                       'rhone-alpes-latest.osm.pbf',
+                                                                       'pays-de-la-loire-latest.osm.pbf',
+                                                                       'bretagne-latest.osm.pbf',
+                                                                       'alsace-latest.osm.pbf',
+                                                                       'aquitaine-latest.osm.pbf',
+                                                                       'auvergne-latest.osm.pbf',
+                                                                       'bourgogne-latest.osm.pbf',
+                                                                       'centre-latest.osm.pbf',
+                                                                       'champagne-ardenne-latest.osm.pbf',
+                                                                       'languedoc-roussillon-latest.osm.pbf',
+                                                                       'limousin-latest.osm.pbf',
+                                                                       'lorraine-latest.osm.pbf',
+                                                                       'midi-pyrenees-latest.osm.pbf',
+                                                                       'poitou-charentes-latest.osm.pbf',
+                                                                       'corse-latest.osm.pbf',
+                                                                       )),
+        (0, 43300000, 'https://download.geofabrik.de/europe/france/', ('franche-comte-latest.osm.pbf',
+                                                                       'nord-pas-de-calais-latest.osm.pbf',
+                                                                       )),
+    ]
+
+    lastId, startId = 0, 0
+    for entry in lst:
+        if entry[0] != 0:
+            lastId, startId = ProcessGeoFabrik(entry[2], entry[3], entry[1])
+            print(f'from {startId} to {lastId} {entry[3]}')
+
 
 if __name__ == '__main__':
 
-    #    q = 'is_in(46.77779,7.16022)->.a;area.a[boundary=administrative][admin_level=6];(node(area););( ._;<;<<;);'
-    #    doOverpass(r'.\Marly.osm', query=q)
-    #    mkgmap(r'.\Marly.osm', hasRoute=False)
+    #q = 'is_in(46.77779,7.16022)->.a;area.a[boundary=administrative][admin_level=6];(node(area););( ._;<;<<;);'
+    #doOverpass(r'.\Marly.osm', query=q)
+    #mkgmap(r'.\Marly.osm', hasRoute=True, styleDir=r'\Usr\Maps\Styles\e20x', typeFile=r'\Usr\Maps\Styles\e20x.typ', mapId=94100000)
     #    mkgmap(r'.\Marly.osm', styleDir=r'\Usr\Maps\Styles\e20', typeFile=r'\Usr\Maps\Styles\e20.typ')
     #    files = []
     #    files.append(mkMap(r'.\CH-AR.osm', 43600001, query=r'area["ISO3166-2"="CH-AR"];(node(area););( ._;<;<<;);'))
@@ -241,82 +295,13 @@ if __name__ == '__main__':
     #        mkgmap(template, styleDir=os.path.join(StyleDir, 'e20x'), typeFile=os.path.join(StyleDir, 'e20x.typ'), mapDescription='GR5Sud_E20')
 
     if True:
-        urlRoot = 'https://download.geofabrik.de/europe/'
-        startId = ProcessGeoFabrik(urlRoot, 'switzerland-latest.osm.pbf', 44100001)
-
-    if False:
-        urlRoot = 'https://download.geofabrik.de/europe/'
-        startId = ProcessGeoFabrik(urlRoot, 'france-latest.osm.pbf', 43610001)
-
-    if True:
-        urlRoot = 'https://download.geofabrik.de/europe/italy/'
-        osmFiles = ('nord-est-latest.osm.pbf',
-            'nord-ovest-latest.osm.pbf',
-            'centro-latest.osm.pbf')
-        startId = ProcessGeoFabrik(urlRoot, osmFiles, 43900001)
-
-    if False:
         # https://extract.bbbike.org?sw_lng=13.215&sw_lat=41.951&ne_lng=19.964&ne_lat=46.231&format=osm.pbf&coords=14.962%2C43.213%7C17.114%2C42.102%7C19.86%2C41.951%7C19.964%2C42.825%7C17.575%2C44.192%7C14.373%2C46.231%7C13.215%2C45.478%7C13.979%2C44.309&city=Yougoslavie&lang=en
-        template = mkgsplit(os.path.join(dnldDir1, 'planet_15.138_43.277_e8b7a8d8.osm.pbf'))
+        template = mkgsplit(os.path.join(dnldDir1, 'planet_15.731_43.069_8e27d44d.osm.pbf'),description='Yougoslavie',
+             mapId=93850000)
         if template is not None:
-            startId = updateMapId(template, 43700001)
-            # mkgmap(template, styleDir=os.path.join(StyleDir, 'e10'), typeFile=os.path.join(StyleDir, 'e10.typ'), mapDescription='E10_Yougoslavie')
-            # startId = updateMapId(template, startId)
-            mkgmap(template, styleDir=os.path.join(StyleDir, 'e20x'), typeFile=os.path.join(StyleDir, 'e20x.typ'),
+            mkgmap(template,
+                   styleDir=os.path.join(StyleDir, 'e20x'), typeFile=os.path.join(StyleDir, 'e20x.typ'),
                    mapDescription='E20_Yougoslavie')
 
-    if True:
-        urlRoot = 'https://download.geofabrik.de/europe/france/'
-        osmFiles = ('franche-comte-latest.osm.pbf',
-                    'haute-normandie-latest.osm.pbf',
-                    'basse-normandie-latest.osm.pbf',
-                    'ile-de-france-latest.osm.pbf',
-                    'nord-pas-de-calais-latest.osm.pbf',
-                    'picardie-latest.osm.pbf',
-                    'provence-alpes-cote-d-azur-latest.osm.pbf',
-                    'rhone-alpes-latest.osm.pbf',
-                    'pays-de-la-loire-latest.osm.pbf',
-                    'bretagne-latest.osm.pbf',
-                    'alsace-latest.osm.pbf',
-                    'aquitaine-latest.osm.pbf',
-                    'auvergne.html',
-                    'bourgogne-latest.osm.pbf',
-                    'centre-latest.osm.pbf',
-                    'champagne-ardenne-latest.osm.pbf',
-                    'languedoc-roussillon-latest.osm.pbf',
-                    'limousin-latest.osm.pbf',
-                    'lorraine-latest.osm.pbf',
-                    'midi-pyrenees-latest.osm.pbf',
-                    'poitou-charentes-latest.osm.pbf',
-                    'corse-latest.osm.pbf',
-                    )
-        startId = ProcessGeoFabrik(urlRoot, osmFiles, 43300001)
-
-    if False:
-        urlRoot = 'https://download.geofabrik.de/europe/germany/'
-        osmFiles = ('baden-wuerttemberg-latest.osm.pbf',
-                    'bayern-latest.osm.pbf',
-                    'https://download.geofabrik.de/europe/germany/saarland-latest.osm.pbf',
-                    'https://download.geofabrik.de/europe/germany/rheinland-pfalz-latest.osm.pbf'
-                    )
-        startId = ProcessGeoFabrik(urlRoot, osmFiles, 4440001)
- 
-    if False:
-        urlRoot = 'https://download.geofabrik.de/europe/'
-        startId = ProcessGeoFabrik(urlRoot, 'austria-latest.osm.pbf', 44300001)
- 
-    if False:
-        urlRoot = 'https://download.geofabrik.de/europe/'
-        startId = ProcessGeoFabrik(urlRoot, 'cyprus-latest.osm.pbf', 43900001)
-
-    if False:
-        urlRoot = 'https://download.geofabrik.de/australia-oceania/'
-        osmFiles = ('australia-latest.osm.pbf',
-                    'new-zealand-latest.osm.pbf',
-                    )
-        startId = ProcessGeoFabrik(urlRoot, osmFiles, 46100001)
-
+    GeoFabrik()
     cleanup()
-
-# https://download.geofabrik.de/africa/madagascar-latest.osm.pbf
-# https://download.geofabrik.de/africa/mauritius-latest.osm.pbf
